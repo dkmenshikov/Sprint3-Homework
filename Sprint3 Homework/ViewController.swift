323b7e503a0b60b960289e3cd536c22c3c9152a9
Dima M <dkmenshikov@gmail.com>
2023-11-13 16:02:08 +0300

    Реализована базовая задача домашней работы

--- Sprint3 Homework/ViewController.swift
+++ Sprint3 Homework/ViewController.swift
@@ -12,8 +12,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
+    
+    private var counterValue: Int = 0
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBOutlet weak var incButton: UIButton!
+    
+    @IBAction func incButtonDidTap(_ sender: Any) {
+        print("Нажатие +")
+        counterValue += 1
+        counterLabel.text = "Значение счетчика: \(counterValue)"
+    }
+    
 
 }
 
